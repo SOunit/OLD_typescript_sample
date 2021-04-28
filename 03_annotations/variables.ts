14e4f188e03717725a9e41992f4bb6fdc6ebf3ca
SOUnit <workbookwork7@gmail.com>
2021-04-28 20:49:55 +0900

    added annotation sample

---
+++ 03_annotations/variables.ts
@@ -0,0 +1,8 @@
+const apples: number = 5;
+const speed: string = 'fast';
+const hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+let now: Date = new Date();
