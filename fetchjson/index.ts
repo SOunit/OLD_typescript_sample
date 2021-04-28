276b38d69f23f790d04e03e319257722d0a5c962
SOUnit <workbookwork7@gmail.com>
2021-04-28 12:52:15 +0900

    added typescript interface sample

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,4 +2,20 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then(res=>{console.log(res.data)});
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((res) => {
+  const todo = res.data as Todo;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+  console.log(`
+  id: ${id} 
+  title: ${title} 
+  completed: ${completed}
+  `);
+});
