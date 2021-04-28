cc351f0952c947c38001d1524f36f8546096da57
SOUnit <workbookwork7@gmail.com>
2021-04-28 21:47:31 +0900

    added more annotation sample

--- 03_annotations/variables.ts
+++ 03_annotations/variables.ts
@@ -19,3 +19,30 @@ let car: Car = new Car();
 
 // object literal
 let point: { x: number; y: number } = { x: 10, y: 20 };
+
+// when to use annotations
+// 1. function that returns any type
+const json = '{"x": 10, "y": 20}';
+const coodinates: { x: number; y: number } = JSON.parse(json);
+console.log(coodinates);
+
+// 2. when we declare a variable on one line
+//  and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3. variable whose type cannot be infered correctly
+let testNums = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < testNums.length; i++) {
+  if (testNums[i] > 0) {
+    numberAboveZero = testNums[i];
+  }
+}
