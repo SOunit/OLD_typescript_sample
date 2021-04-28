42da4d90b03b1f4d24f3a01254f75402e5dd4b69
SOUnit <workbookwork7@gmail.com>
2021-04-28 13:25:28 +0900

    added type sample

---
+++ 02_features/types.ts
@@ -0,0 +1,15 @@
+// type sample
+
+// type = Date
+const today = new Date();
+today.getMonth();
+
+// erro on invalid method
+// today.NotExistMethod();
+
+// type = { age: 20 }
+const person = { age: 20 };
+
+// type = Color
+class Color {}
+const color = new Color();
