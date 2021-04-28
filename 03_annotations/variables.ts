8cb0e3cc4df9fd93b2826006ef5d484623412509
SOUnit <workbookwork7@gmail.com>
2021-04-28 21:02:14 +0900

    added more annotation sample

--- 03_annotations/variables.ts
+++ 03_annotations/variables.ts
@@ -5,4 +5,17 @@ const hasName: boolean = true;
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
+// built in object
 let now: Date = new Date();
+
+// array
+let colors: string[] = ['green', 'blue', 'yellow'];
+let numbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// classes
+class Car {}
+let car: Car = new Car();
+
+// object literal
+let point: { x: number; y: number } = { x: 10, y: 20 };
