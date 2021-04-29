7bd62d7b5e8cfcda97a466f61e040bbe408c576e
SOUnit <workbookwork7@gmail.com>
2021-04-29 10:20:57 +0900

    added tuple sample

---
+++ 02_features/tuples.ts
@@ -0,0 +1,12 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+const pepsi: [string, boolean, number] = ['brown', true, 40];
+
+// type alias
+type Drink = [string, boolean, number];
+const cola: Drink = ['brown', true, 30];
+const tea: Drink = ['brown', false, 0];
