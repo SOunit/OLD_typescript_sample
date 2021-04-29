c45cb5e54c6d3d265d62c5985625cbdd6aedfc9c
SOUnit <workbookwork7@gmail.com>
2021-04-29 21:40:49 +0900

    added company class

--- 03_maps/src/index.ts
+++ 03_maps/src/index.ts
@@ -1,7 +1,11 @@
 import { User } from './User';
+import { Company } from './Company';
 
 // parcel index.html
 // parcel change index.ts to index.js
 
 const user = new User();
 console.log(user);
+
+const company = new Company();
+console.log(company);
