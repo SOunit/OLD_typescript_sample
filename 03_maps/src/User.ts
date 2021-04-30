c790278d2471a8bc02f1e6b999370252fa060894
SOUnit <workbookwork7@gmail.com>
2021-04-30 13:11:23 +0900

    added comments of marker, added impletent interface

--- 03_maps/src/User.ts
+++ 03_maps/src/User.ts
@@ -1,20 +1,27 @@
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
 export const red = 'red';
 // in TypeScript, default export is not recommended
 // export default 'red';
 
-export class User {
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = 'red';
+
   constructor() {
     this.name = faker.name.firstName();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
+  }
+
+  markerContent(): string {
+    return `Name: ${this.name}`;
   }
 }
