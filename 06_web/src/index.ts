f5fb7ec53c0c389406e252d9cea162c9056a025b
SOUnit <workbookwork7@gmail.com>
2021-05-03 18:07:15 +0900

    added on action, creating event array

--- 06_web/src/index.ts
+++ 06_web/src/index.ts
@@ -1,13 +1,9 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Jack', age: 38 });
-console.log(user.get('name'));
-console.log(user.get('age'));
 
-user.set({ name: 'Kate', age: 39 });
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {});
+user.on('change', () => {});
+user.on('click', () => {});
 
-user.set({ name: 'Kevin' });
-console.log(user.get('name'));
-console.log(user.get('age'));
+console.log(user);
