853ddabd1c4b0f78c3b7daeedba08d34f3c4f005
SOUnit <workbookwork7@gmail.com>
2021-05-03 19:56:36 +0900

    added save to user

--- 06_web/src/index.ts
+++ 06_web/src/index.ts
@@ -1,5 +1,8 @@
 import { User } from './models/User';
 
 const user = new User({ id: 1 });
-user.fetch();
-console.log(user);
+user.set({ name: 'Rebecca', age: 14 });
+user.save();
+
+const newUser = new User({ name: 'Jack', age: 0 });
+newUser.save();
