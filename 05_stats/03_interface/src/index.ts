a5cbe6281323c980d099aeb3df5d7ed3728db8f9
SOUnit <workbookwork7@gmail.com>
2021-05-03 11:59:58 +0900

    added project to add new fix

---
+++ 05_stats/03_interface/src/index.ts
@@ -0,0 +1,25 @@
+import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
+
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+// analyze data
+let manUnitedWins = 0;
+
+for (let match of matchReader.matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+// report data
+console.log(`Man United won ${manUnitedWins} games`);
