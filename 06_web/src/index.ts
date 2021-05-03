d0e1da0107e457945277f38c9b676ba6012f47e9
SOUnit <workbookwork7@gmail.com>
2021-05-03 19:03:34 +0900

    added event trigger

--- 06_web/src/index.ts
+++ 06_web/src/index.ts
@@ -2,8 +2,17 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Jack', age: 38 });
 
-user.on('change', () => {});
-user.on('change', () => {});
-user.on('click', () => {});
+user.on('change', () => {
+  console.log('change #1');
+});
+user.on('change', () => {
+  console.log('change #2');
+});
+user.on('click', () => {
+  console.log('click');
+});
+user.on('save', () => {
+  console.log('save');
+});
 
-console.log(user);
+user.trigger('test');
