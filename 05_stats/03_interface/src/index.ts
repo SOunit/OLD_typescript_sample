6befcc29bed84ceb888656730ed02ae2d957a3ad
SOUnit <workbookwork7@gmail.com>
2021-05-03 13:49:34 +0900

    added static instance generator method

--- 05_stats/03_interface/src/index.ts
+++ 05_stats/03_interface/src/index.ts
@@ -1,16 +1,7 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
-import { HtmlReport } from './reportTargets/HtmlReport';
 
-// Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
-
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
 const summary = Summary.winsAnalysisWithHtmlReport('Man United');
