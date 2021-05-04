e473a702b17415f2952ed17433ca77c0aa6f659f
SOUnit <workbookwork7@gmail.com>
2021-05-04 12:42:48 +0900

    added sync class

---
+++ 06_web/src/models/Sync.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosPromise } from 'axios';
+import { UserProps } from './User';
+
+export class Sync {
+  constructor(public baseUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.baseUrl}/${id}`);
+  }
+
+  save(data: UserProps): AxiosPromise {
+    const { id } = data;
+
+    if (id) {
+      return axios.put(`${this.baseUrl}/${id}`, data);
+    } else {
+      return axios.post(this.baseUrl, data);
+    }
+  }
+}
