acfea368598ffbd55c4596a73e7caebca4c7204f
SOUnit <workbookwork7@gmail.com>
2021-05-04 10:47:49 +0900

    extracting event logic from user

--- 06_web/src/models/User.ts
+++ 06_web/src/models/User.ts
@@ -6,11 +6,7 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
-
   constructor(private data: UserProps) {}
 
   get(propName: string): string | number {
@@ -22,24 +18,6 @@ export class User {
     // copy all prop from 2nd argument to 1st argument
     // if prop exists, override the prop
     Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach((callback) => {
-      callback();
-    });
   }
 
   fetch(): void {
