a5738cf1a1d42b8830c5f6c83c309b6d573cb187
SOUnit <workbookwork7@gmail.com>
2021-05-05 18:48:58 +0900

    added generic constraints

--- 06_web/src/views/View.ts
+++ 06_web/src/views/View.ts
@@ -1,7 +1,8 @@
-import { User } from '../models/User';
+import { Model } from '../models/Model';
+import { UserProps } from '../models/User';
 
-export abstract class View {
-  constructor(public parent: Element, public model: User) {
+export abstract class View<T extends Model<K>, K> {
+  constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
 
