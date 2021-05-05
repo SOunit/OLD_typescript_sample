5d574323611b5066789a611f4030bcbc43938a88
SOUnit <workbookwork7@gmail.com>
2021-05-05 12:47:22 +0900

    added event handler

--- 06_web/src/views/UserForm.ts
+++ 06_web/src/views/UserForm.ts
@@ -1,18 +1,34 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  onButtonClick(): void {
+    console.log('hi there');
+  }
+
+  // old way to handle event before react, vue, anglar...
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+    };
+  }
+
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
+        <input />
+        <button>Click Me</button>
       </div>
       `;
   }
 
   render(): void {
+    // create html element
     const templateElement = document.createElement('template');
+    // convert string to html element
     templateElement.innerHTML = this.template();
 
+    // add html element to dom
     this.parent.append(templateElement.content);
   }
 }
