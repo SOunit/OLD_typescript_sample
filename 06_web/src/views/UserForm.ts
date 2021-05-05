c22d3bd7b9a147e684262f8e7dc6b7c8ac3c49f2
SOUnit <workbookwork7@gmail.com>
2021-05-05 18:35:42 +0900

    added abstract view class

--- 06_web/src/views/UserForm.ts
+++ 06_web/src/views/UserForm.ts
@@ -1,16 +1,7 @@
 import { User } from '../models/User';
+import { View } from '../views/View';
 
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.bindModel();
-  }
-
-  bindModel(): void {
-    this.model.on('change', () => {
-      this.render();
-    });
-  }
-
+export class UserForm extends View {
   // old way to handle event before react, vue, anglar...
   eventsMap(): { [key: string]: () => void } {
     return {
@@ -43,33 +34,5 @@ export class UserForm {
         <button class="set-age">Set Random Age</button>
       </div>
       `;
-  }
-
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-
-    for (let eventKey in eventsMap) {
-      // click:button -> click, button
-      const [eventName, selector] = eventKey.split(':');
-
-      fragment.querySelectorAll(selector).forEach((element) => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
-  }
-
-  render(): void {
-    // clear all html
-    this.parent.innerHTML = '';
-
-    // create html element
-    const templateElement = document.createElement('template');
-    // convert string to html element
-    templateElement.innerHTML = this.template();
-    // add events
-    this.bindEvents(templateElement.content);
-
-    // add html element to dom
-    this.parent.append(templateElement.content);
   }
 }
---
+++ 06_web/src/views/View.ts
@@ -0,0 +1,44 @@
+import { User } from '../models/User';
+
+export abstract class View {
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  abstract eventsMap(): { [key: string]: () => void };
+  abstract template(): string;
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      // click:button -> click, button
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  render(): void {
+    // clear all html
+    this.parent.innerHTML = '';
+
+    // create html element
+    const templateElement = document.createElement('template');
+    // convert string to html element
+    templateElement.innerHTML = this.template();
+    // add events
+    this.bindEvents(templateElement.content);
+
+    // add html element to dom
+    this.parent.append(templateElement.content);
+  }
+}
